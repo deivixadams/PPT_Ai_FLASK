ccccadd7dc3a42e86d6ef51955e1bf03061cffa2
Deivis Adames <deivisadames@gmail.com>
2023-10-07 08:03:58 -0400

    fotos, nombre archivo sin caracteres no admisibles

--- models/(original) presentation_creator copy.py
+++ models/(original) presentation_creator copy.py
@@ -1,29 +1,25 @@
 import os
 import requests
-from pptx import Presentation
-from pptx.util import Inches, Pt
+from pptx import Presentation 
+from pptx.util import Inches # Para cambiar el tamaño de la imagen
+from pptx.util import Pt # Para cambiar el tamaño de la fuente
+#from pptx.util import RGBColor # Para cambiar el color de la fuente
 from datetime import datetime
 from models.openai_langchain import OpenAiLC
 import logging
-import tempfile
-import shutil
-import re
-
-'''
-Mi sistema genera fotos con imagenes
-libre de Derechos de Autor
-'''
+import tempfile # Para crear directorio temporal
+import shutil # Para eliminar directorio temporal
 
 class PresentationCreator:
     def __init__(self):
-        self.llmlc = OpenAiLC()
+        self.llmlc = OpenAiLC() 
         logging.basicConfig(level=logging.DEBUG)
-
+        
     def crear_presentacion(self, dataslide, tema):
         if dataslide is None:
             print("Error: 'dataslide' no puede ser None")
             return None
-
+        
         prs = Presentation()
 
         # Set the slide width and height to the default widescreen dimensions
@@ -32,49 +28,39 @@ class PresentationCreator:
 
         ahora = datetime.now()
         fecha_hora = ahora.strftime("fecha-%d-%m-%Y_hora-%H-%M-%S")
-
+        
         for slide_info in dataslide:
-            slide_layout = prs.slide_layouts[5]  # Título Sólo layout
+            slide_layout = prs.slide_layouts[1]  #antes estaba el 5
             slide = prs.slides.add_slide(slide_layout)
-
-            # Agregar título
+            
+            # Add title and text
             title_box = slide.shapes.title
             title_box.text = slide_info["title"]
-
-            # Agregar texto a la izquierda
-            left_text = Inches(1)
-            top_text = Inches(2)  # Ajusta según sea necesario
-            width_text = Inches(5)
-            height_text = Inches(6)
-            txBox = slide.shapes.add_textbox(left_text, top_text, width_text, height_text)
+            left = Inches(1)
+            top = Inches(1.5)
+            width = height = Inches(8)
+            txBox = slide.shapes.add_textbox(left, top, width, height)
             tf = txBox.text_frame
             tf.word_wrap = True
             p = tf.add_paragraph()
-            p.font.size = Pt(16) #ajusa el tamaño de la letra
+            p.font.size = Pt(14) 
             p.text = slide_info["content"]
 
-            # Agregar imagen a la derecha
             try:
                 image_url = self.llmlc.generar_imagen(slide_info["title"])
                 response = requests.get(image_url, stream=True)
-                with tempfile.NamedTemporaryFile(delete=False, suffix='.png') as temp_file: # Crea un archivo temporal
-                    shutil.copyfileobj(response.raw, temp_file) # Copia la imagen en el archivo temporal
-                left_img = Inches(6)  # Ajusta según sea necesario
-                top_img = Inches(2)  # Ajusta según sea necesario
-                width_img = Inches(3)
-                height_img = Inches(3)
-                pic = slide.shapes.add_picture(temp_file.name, left_img, top_img, width_img, height_img)
+                with tempfile.NamedTemporaryFile(delete=False, suffix='.png') as temp_file:
+                    shutil.copyfileobj(response.raw, temp_file)
+                left = Inches(1)
+                top = Inches(2.5)
+                pic = slide.shapes.add_picture(temp_file.name, left, top, width=Inches(3), height=Inches(3))
                 os.unlink(temp_file.name)  # Eliminar el archivo temporal
             except Exception as e:
                 logging.error(f"Error al buscar imagen en OpenAi: {e}")
                 print(f"Error al buscar imagen en OpenAi: {e}")
                 image_url = None
 
-        # Prepara el tema y la fecha_hora para ser seguros como nombres de archivo
-        tema_safe = re.sub(r'[<>:"/\\|?*]', '', tema)  # Elimina caracteres no permitidos
-        fecha_hora_safe = re.sub(r'[<>:"/\\|?*]', '', fecha_hora)  # Elimina caracteres no permitidos
-        nombre_archivo = f"{tema_safe}-{fecha_hora_safe}.pptx"
-        #nombre_archivo = f"{tema}-{fecha_hora}.pptx"
+        nombre_archivo = f"{tema}-{fecha_hora}.pptx"
         ruta_guardado = os.path.join("RESULTADO", nombre_archivo)
         try:
             prs.save(ruta_guardado)
@@ -83,3 +69,4 @@ class PresentationCreator:
         except PermissionError:
             print("No se pudo crear el archivo, probablemente esté abierto.")
             return None
+
